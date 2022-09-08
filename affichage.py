8b7f77f5344eeb8bd9b86bc765e15ec6ac3ca44b
LCoussy <90974863+LCoussy@users.noreply.github.com>
2022-09-08 23:26:48 +0200

    Update affichage.py

--- affichage.py
+++ affichage.py
@@ -1,7 +1,7 @@
 def affichage(x):
     nb = str(x)
     fichier = "ASCII"
-    f = open(fichier + nb + ".txt","r")
+    f = open("LifeStage/" + fichier + nb + ".txt","r")
     a = f.read()
     print(a)
     f.close
